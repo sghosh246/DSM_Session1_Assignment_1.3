f7371bcbfe18891e6dfab83c22572f079f01c172
sghosh246 <sghosh246@gmail.com>
2018-04-13 13:00:56 +0530

    Add files via upload

---
+++ reverseorder.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 10:05:21 2018
+@author: souravg
+To do : Accept First and Last name and print them in reverse order.
+"""
+fname = input("Enter your First Name : ")
+lname = input("Enter your Last Name : ")
+print("Your FullName is %s and the reverse order of your FullName is %s" %(fname+' '+lname,lname+' '+fname))
